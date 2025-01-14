d0a3b6e8fb09956f56f76018cfe30242d8582afc
uttamsaravadiya <uttamsaravadiya1224.com>
2025-01-14 16:43:12 +0530

    colur

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <AuthProvider>
       <Router>
-        <div className="min-h-screen bg-gray-100">
+        <div className="min-h-screen bg-gray-50">
           <Navbar />
           <Routes>
             <Route path="/" element={<Home />} />

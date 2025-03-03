aa25df3d69913f7ec9e58a3c02384ed3b4043983
TanmayJotangia <jotangiatanmay40@gmail.com>
2025-03-04 00:40:35 +0530

    Added Assests

---
+++ src/assets/assets.js
@@ -0,0 +1,156 @@
+import Mobile1 from "./Mobile1.png";
+import Mobile2 from "./Mobile2.png";
+import Mobile3 from "./Mobile3.png";
+import Mobile4 from "./Mobile4.png";
+import Mobile5 from "./Mobile5.png";
+import Mobile6 from "./Mobile6.png";
+import tv_1 from "./tv_1.png";
+import tv_2 from "./tv_2.png";
+import tv_3 from "./tv_3.png";
+import tv_4 from "./tv_4.png";
+import tv_5 from "./tv_5.png";
+import tv_6 from "./tv_6.png";
+
+export const products = [
+  {
+    id: 1,
+    name: "Samsung Galaxy Z Flip 6",
+    description:
+      "Samsung Galaxy Z Flip 6 5G AI Smartphone (Black, 12GB RAM, 256GB Storage) ",
+    size: "12GB + 256GB",
+    price: 81990,
+    image: [Mobile1],
+    category: "Mobile",
+    featured: true,
+    rating: 4.5,
+    countInStock: 10,
+  },
+  {
+    id: 2,
+    name: "Vivo X60 Pro",
+    description:
+      "Vivo X60 Pro (Shimmer Blue, 12GB RAM, 256GB Storage) with No Cost EMI/Additional Exchange Offers",
+    size: "8GB + 128GB",
+    price: 14999,
+    image: [Mobile2],
+    category: "Mobile",
+    featured: false,
+    rating: 4.5,
+    countInStock: 10,
+  },
+  {
+    id: 3,
+    name: "Samsung Galaxy Z Flip 3",
+    description:
+      "Samsung Galaxy Z Flip 3 5G AI Smartphone (Yellow, 8GB RAM, 256GB Storage)",
+    size: "8GB + 256GB",
+    price: 84999,
+    image: [Mobile3],
+    category: "Mobile",
+    featured: false,
+    rating: 3.5,
+    countInStock: 5,
+  },
+  {
+    id: 4,
+    name: "Nothing Phone (2a)",
+    description:
+      "Nothing Phone (2a) 5G AI Smartphone (Black, 12GB RAM, 256GB Storage)",
+    size: "12GB + 256GB",
+    price: 29999,
+    image: [Mobile4],
+    category: "Mobile",
+    featured: true,
+    rating: 4.5,
+    countInStock: 6,
+  },
+  {
+    id: 5,
+    name: "Google Pixel 7 Pro",
+    description:
+      "Google Pixel 7 Pro 5G AI Smartphone (Black, 12GB RAM, 512GB Storage)",
+    size: "12GB + 512GB",
+    price: 99999,
+    image: [Mobile5],
+    category: "Mobile",
+    featured: false,
+    rating: 4.0,
+  },
+  {
+    id: 6,
+    name: "Apple iPhone 14 Plus",
+    description: "Apple iPhone 14 Plus (Snow, iOS 16, 1TB Storage)",
+    size: "12GB + 1TB",
+    price: 199999,
+    image: [Mobile6],
+    category: "Mobile",
+    featured: true,
+    rating: 5.0,
+  },
+  {
+    id: 7,
+    name: "Croma CREL ",
+    description: "Croma CREL7321 32 inch HD Ready LED TV",
+    size: "32 inch",
+    price: 8490,
+    image: [tv_1],
+    category: "TV",
+    featured: false,
+    rating: 3.0,
+  },
+  {
+    id: 8,
+    name: "TCL Google Tv",
+    description: "TCL Google Tv 40 inch Full HD LED Smart TV",
+    size: "40 inch",
+    price: 13990,
+    image: [tv_2],
+    category: "TV",
+    featured: true,
+    rating: 4.0,
+  },
+  {
+    id: 9,
+    name: "Intex LED TV",
+    description: "Intex LED TV 32 inch Full HD LED Smart TV",
+    size: "32 inch",
+    price: 13457,
+    image: [tv_3],
+    category: "TV",
+    featured: false,
+    rating: 3.5,
+  },
+  {
+    id: 10,
+    name: "DU7660 Crystal 4K UHD Smart TV",
+    description: "DU7660 Crystal 4K UHD Smart TV 55 inch",
+    size: "55 inch",
+    price: 8999,
+    image: [tv_4],
+    category: "TV",
+    featured: true,
+    rating: 4.5,
+  },
+  {
+    id: 11,
+    name: "Samsung QLED Ultra HD Smart Tizen TV",
+    description: "Samsung QLED Ultra HD Smart Tizen TV 65 inch",
+    size: "65 inch",
+    price: 159999,
+    image: [tv_5],
+    category: "TV",
+    featured: true,
+    rating: 5.0,
+  },
+  {
+    id: 12,
+    name: "Croma HD LED Smart TV",
+    description: "Croma HD LED Smart TV 43 inch with Bezel Less Design",
+    size: "43 inch",
+    price: 19999,
+    image: [tv_6],
+    category: "TV",
+    featured: true,
+    rating: 4.5,
+  },
+];
